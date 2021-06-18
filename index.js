d6df601004d2f8c59d3407c6d5a45ea8ba8d114b
Lucas Moreno <moreno.lucas.82300@hotmail.fr>
2021-06-18 17:24:59 +0200

    architecture api

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello")
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+require("dotenv").config()
+const process = require("process")
+
+const app = express()
+
+var corsOptions = {
+  origin: "http://localhost:80",
+}
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require("./src/routes/routes.js")(app)
+
+// set port, listen for requests
+const PORT = process.env.API_PORT || 80
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
+
+module.exports = app
